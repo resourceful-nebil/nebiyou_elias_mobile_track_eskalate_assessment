d8ebd9c9974db5d727912aa1ddd23ace53956b18
Nebil <resourceful.nebil@gmail.com>
2025-06-19 13:00:05 +0300

    Add Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:assessment/presentation/pages/favorites_page.dart';
+import 'package:assessment/presentation/bloc/country_event.dart';
+import 'package:assessment/presentation/pages/nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'injection.dart';
 import 'presentation/bloc/country_bloc.dart';
-import 'presentation/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await init(); // Call the async init function
+  await init(); // Or configureDependencies() if you renamed it
   runApp(MyApp());
 }
 
@@ -15,17 +15,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => sl<CountryBloc>(),
+      create: (context) => sl<CountryBloc>()..add(LoadCountries()),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Country App',
         theme: ThemeData(primarySwatch: Colors.blue),
-        initialRoute: '/',
-        onGenerateRoute: (settings) {
-          if (settings.name == '/') return MaterialPageRoute(builder: (_) => HomePage());
-          if (settings.name == '/favorites') return MaterialPageRoute(builder: (_) => FavoritesPage());
-          return null;
-        },
+        // Navigate to MainScreen first
+        home: MainScreen(), 
       ),
     );
   }
